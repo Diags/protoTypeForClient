31318c36316f26888e43eb935cc301746e6406cd
dsylla <diaguily.sylla@agixis.com>
2019-02-19 17:09:19 +0100

    Initial commit

---
+++ src/main/java/app/agixis/com/protoTypeForClient/ProtoTypeForClientApplication.java
@@ -0,0 +1,13 @@
+package app.agixis.com.protoTypeForClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoTypeForClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoTypeForClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/agixis/com/protoTypeForClient/ProtoTypeForClientApplicationTests.java
@@ -0,0 +1,16 @@
+package app.agixis.com.protoTypeForClient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProtoTypeForClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
